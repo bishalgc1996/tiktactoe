b5bc802a6beb6e7f26cd78a66640bc8d91647fe3
bishalgc1996 <bishal.gc.1996@gmail.com>
2022-02-19 22:06:14 +0545

    Adding styles and js files

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Main Js File
+ */
+
+let crossButton = document.getElementById("icon-choice-multiply");
+let zeroButton = document.getElementById("icon-choice-zero");
+
+function valueCross() {
+  const getValue = crossButton.value;
+}
