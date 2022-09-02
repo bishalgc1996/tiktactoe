6de6c1fb5a16498f489f3d3e9d405a143a881429
bishalgc1996 <bishal.gc.1996@gmail.com>
2022-09-02 16:06:07 +0545

    Adding code for background change of selected value

--- index.js
+++ index.js
@@ -13,35 +13,48 @@ class Player {
     console.log(`Value ${this.valueButton}`);
     let selectedBox = document.querySelector(".first-row td");
     console.log(typeof selectedBox);
-    console.log(selectedBox);
-    selectedBox.innerText = this.valueButton;
+    console.log(selectedBox.innerText);
 
-    if (!selectedBox.innerText) {
-      let selectedBox = document.querySelector(".first-row td");
+    if (selectedBox.innerText != "") {
+      console.log("Not Empty");
+      let selectedBox = document.querySelector(".second-row td");
+      selectedBox.innerText = this.valueButton;
+    } else {
+      selectedBox.innerText = this.valueButton;
     }
   }
 }
 
 let crossButton = document.getElementById("icon-choice-multiply");
 let zeroButton = document.getElementById("icon-choice-zero");
+let crossbuttonCom = document.getElementById("icon-choice-multiply-com");
+let zerobuttonCom = document.getElementById("icon-choice-zero-com");
 
 console.log(crossButton.innerText);
 console.log(zeroButton.innerText);
 
 window.addEventListener("DOMContentLoaded", (event) => {
   if (crossButton.innerText === "X") getdomValue(crossButton.innerText);
+  // if (crossbuttonCom.innerText === "X") getdomValue(crossbuttonCom.innerText);
   if (zeroButton.innerText === "0") getdomValue(zeroButton.innerText);
+ // if (zerobuttonCom.innerText === "0") getdomValue(zerobuttonCom.innerText);
 
   function getdomValue(buttonValue) {
     if (buttonValue === "X") {
       const playerOne = new Player(buttonValue);
       crossButton.addEventListener("click", function () {
         playerOne.changeboxValue();
+        crossButton.style.backgroundColor = '#00b451';
+        zeroButton.style.display = 'none';
+        crossbuttonCom.style.display  = 'none'
       });
     } else {
       const playerTwo = new Player(buttonValue);
       zeroButton.addEventListener("click", function () {
         playerTwo.changeboxValue();
+        zeroButton.style.backgroundColor = '#955f5f';
+        crossButton.style.display = 'none';
+        zerobuttonCom.style.display  = 'none'
       });
     }
   }
